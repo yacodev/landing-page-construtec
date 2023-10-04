8e0e34153da3d0754497a43cd21aec59e6f034c9
yacodev <cyaco33@gmail.com>
2023-10-04 11:25:08 -0500

    update deadline to end  promotion

--- components/SaasLanding/RegistrationCampaign.js
+++ components/SaasLanding/RegistrationCampaign.js
@@ -4,11 +4,15 @@ import Link from 'next/link';
 import config from '../../utils/config';
 
 const RegistrationCampaign = () => {
-  const CompletionCount = () => <span>Promoción finalizada</span>;
+  const CompletionCount = () => (
+    <div className='description_campaign mt-4'>
+      <h3>Promoción finalizada</h3>
+    </div>
+  );
 
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
-      return <Completionist />;
+      return <CompletionCount />;
     } else {
       return (
         <h3>
@@ -31,8 +35,8 @@ const RegistrationCampaign = () => {
               <p>
                 Al registrate en la plataforma podrás acceder de manera gratuita
                 al módulo de almacen por un periodo de prueba por 30 días sin
-                costo alguno. Esta promoción solo es valida hasta el 30 de
-                Setiembre del 2023.
+                costo alguno. Esta promoción solo es valida hasta el 31 de
+                Octubre del 2023.
               </p>
             </div>
             <div className='saas-registration-btn mt-4'>
@@ -45,11 +49,11 @@ const RegistrationCampaign = () => {
             <div className='description_campaign mt-4'>
               <p>
                 Aun queda tiempo para registrarte y acceder de manera gratuita a
-                nuestra plataforma, la promoción a un es valida por::
+                nuestra plataforma, la promoción a un es valida por:
               </p>
             </div>
             <div className='count-down'>
-              {<Countdown date={new Date(2023, 8, 30)} renderer={renderer} />}
+              {<Countdown date={new Date(2023, 9, 31)} renderer={renderer} />}
             </div>
           </div>
         </div>
