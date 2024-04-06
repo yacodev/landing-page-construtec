45ed9392fe872ab53101b22065dc1bc47347898c
yacodev <cyaco33@gmail.com>
2024-04-06 16:35:32 -0500

    add Tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,17 @@ class MyDocument extends Document {
     return (
       <Html lang='es' dir={dir}>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5433W6D4)'
+              `,
+            }}
+          />
           <link
             href='https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
             rel='stylesheet'
@@ -33,6 +44,13 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5433W6D4'
+            title='Google Tag Manager'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
           <Main />
           <NextScript />
         </body>
